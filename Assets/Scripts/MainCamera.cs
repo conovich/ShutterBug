6c46d4572f957eaed07a2e4fe6a0dd73e8c84fc8
Gary <likangning93@gmail.com>
2013-09-07 10:54:54 -0400

    added panning for the camera with clicky arrows

--- Assets/Scripts/MainCamera.cs
+++ Assets/Scripts/MainCamera.cs
@@ -10,6 +10,18 @@ public class MainCamera : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (Input.GetMouseButtonDown(0)) {
+			RaycastHit rayCastData = new RaycastHit();
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			if (Physics.Raycast (ray, out rayCastData, 1000.0f)) {
+				if (rayCastData.collider.gameObject.tag == "rightArrow") {
+					gameObject.transform.Rotate(0.0f, 9.0f, 0.0f);
+				}
+				if (rayCastData.collider.gameObject.tag == "leftArrow") {
+					gameObject.transform.Rotate(0.0f, -9.0f, 0.0f);
+				}
+			}
+			
+		}
 	}
 }
