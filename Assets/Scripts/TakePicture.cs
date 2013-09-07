159a1964e2122883c7956b3b653963bea3d0304b
Corey Novich <conovich@gmail.com>
2013-09-07 10:51:57 -0400

    camera excludes guiobject layer in photo

--- Assets/Scripts/TakePicture.cs
+++ Assets/Scripts/TakePicture.cs
@@ -20,7 +20,7 @@ public class TakePicture : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		gameObject.camera.cullingMask = ~(1 << 8);
 	}
 
     void OnPostRender() {
