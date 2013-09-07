df69c438bbbfb7e752f9ec288547de8bc2b7cf7d
Corey Novich <conovich@gmail.com>
2013-09-07 14:52:23 -0400

    committing main camera script...

--- Assets/Scripts/MainCamera.cs
+++ Assets/Scripts/MainCamera.cs
@@ -3,12 +3,10 @@ using System.Collections;
 
 public class MainCamera : MonoBehaviour {
 
-	// Use this for initialization
 	void Start () {
 	
 	}
 	
-	// Update is called once per frame
 	void Update () {
 		if (Input.GetMouseButtonDown(0)) {
 			RaycastHit rayCastData = new RaycastHit();
@@ -21,7 +19,9 @@ public class MainCamera : MonoBehaviour {
 					gameObject.transform.Rotate(0.0f, -9.0f, 0.0f);
 				}
 			}
-			
 		}
 	}
+	
 }
+
+
