f76c44427881c226235fdf2f9bc025a4182bb0a5
Gary <likangning93@gmail.com>
2013-09-08 01:06:08 -0400

    added the ability to walk forwards and backwards

--- Assets/Scripts/MainCamera.cs
+++ Assets/Scripts/MainCamera.cs
@@ -12,11 +12,20 @@ public class MainCamera : MonoBehaviour {
 			RaycastHit rayCastData = new RaycastHit();
 			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
 			if (Physics.Raycast (ray, out rayCastData, 1000.0f)) {
-				if (rayCastData.collider.gameObject.tag == "rightArrow") {
+				GameObject arrow = rayCastData.collider.gameObject;
+				if (arrow.tag == "rightArrow") {
 					gameObject.transform.Rotate(0.0f, 9.0f, 0.0f);
 				}
-				if (rayCastData.collider.gameObject.tag == "leftArrow") {
+				if (arrow.tag == "leftArrow") {
 					gameObject.transform.Rotate(0.0f, -9.0f, 0.0f);
+				}
+				if (arrow.tag == "walkForward") {
+					Vector3 facing = Camera.main.transform.forward;
+					gameObject.transform.position += new Vector3(2 * facing.x, 0.0f, 2 * facing.z);
+				}
+				if (arrow.tag == "walkBack") {
+					Vector3 facing = Camera.main.transform.forward;
+					gameObject.transform.position -= new Vector3(2 * facing.x, 0.0f, 2 * facing.z);
 				}
 			}
 		}
